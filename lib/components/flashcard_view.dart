6e3e291834677a955ccf681126d5702d85929213
Zhasmina Aitmagambetova <aitmazh@Zhasminas-MacBook-Air.local>
2022-12-12 15:33:40 +0600

    rewrote the colors

---
+++ lib/components/flashcard_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '/theme/style_imports.dart';
+
+class FlashCardView extends StatelessWidget {
+  final String? text;
+
+  const FlashCardView({Key? key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      color: StyleImports.colors.container.background,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: StyleImports.colors.text.heading,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Center(
+            child: Text(text!, textAlign: TextAlign.center,),
+          ),
+    );
+  }
+}
